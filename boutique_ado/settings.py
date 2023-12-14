7617695d85be7ecc6b8416a27d54bdf04870c565
estii20 <>
2023-12-14 13:09:33 +0000

    Remove secret key and set debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uz-5)ynn%yghte7p4h5g5ie6fd2i80_(v^zitq_4lta+^(u0d8'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-adot-04497a992b5b.herokuapp.com', '8000-estii20-boutique-ado-v1-fh514d2iae.us2.codeanyapp.com', '8001-estii20-boutique-ado-v1-fh514d2iae.us2.codeanyapp.com', 'localhost']
 
